ba97c1553d4c6f19e826ff5950a0c22f690eb8c4
batajohn <jphernandezr@outlook.com>
2025-07-25 01:39:51 -0500

    priemr lanzamiento para paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="John Hernandez ",                         # Tu nombre
+    author_email="jphernandezr@outlook.com",                 # Tu correo electrónico
+    url="https://github.com/jphernanezr/tercerRepo.git",     # URL del proyecto
+)
